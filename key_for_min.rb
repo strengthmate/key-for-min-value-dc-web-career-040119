4498fe0079ef580847c43c61a18052894b0dc8be
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-26 13:34:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,10 +13,7 @@ def key_for_min_value(name_hash)
   lowest_key
 end
 
-lowest_value/key start at nil. Method then iterates over the hash. if either the lowest_value is nil, 
-or if the current value of lowest value is greater than the value currently being iterated. 
-then the lowest_value (and lowest_key) 
-change to the key/value of the key/value currently
-being iterated.
-
-The method then returns the lowest key, so if the hash is empty then the default value of nil will be returned. #
+The lowest_value and lowest_key start at nil. 
+The method then iterates over the hash.
+During the iteration, if either the lowest_value equals nil, or if the current value of lowest value is greater than the value currently being iterated, then the lowest_value (and lowest_key) change to the key/value of the key/value currently being iterated.
+When the iteration has completed, the method then returns the lowest key. So if the hash is empty, then the default value of nil will be returned.
