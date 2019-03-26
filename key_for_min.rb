3026a9052cca87bfffd358f5589c295e3e5411d5
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-26 13:28:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,4 +13,4 @@ def key_for_min_value(name_hash)
   lowest_key
 end
 
-# lowest_value starts at nil, so in the iteration if
+# lowest_value/key start at nil, so in the iteration the lowest_value (and lowest_key) resets to the value of the key/value currently being iterated if either the lowest_value is nil, or if the current value of lowest value is greater than the value currently being iterated. The method then returns the lowest key, so if the object is empty then the default value of nil will be returned.
